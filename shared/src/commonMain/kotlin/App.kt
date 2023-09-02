6c1477674c8a8fd3990bc775e5aa04e6b839b06e
Nicolas Mahnic <nico.mahnic.ar@gmail.com>
2023-09-02 03:06:47 +0200

    Aspect changes

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,13 +1,13 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
+import presentation.BirdAppTheme
 import presentation.BirdsViewModel
 import presentation.MainScreen
 
 @Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdsViewModel() })
         MainScreen(birdsViewModel)
     }
--- shared/src/commonMain/kotlin/presentation/MainScreen.kt
+++ shared/src/commonMain/kotlin/presentation/MainScreen.kt
@@ -11,13 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import domain.model.BirdImage
@@ -25,9 +28,26 @@ import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 
 @Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(
+            primary = Color.Black
+        ),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp)
+        )
+    ) {
+        content()
+    }
+}
+
+@Composable
 fun MainScreen(viewModel: BirdsViewModel) {
     val uiState by viewModel.uiState.collectAsState()
-
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -41,6 +61,9 @@ fun MainScreen(viewModel: BirdsViewModel) {
         ) {
             for(category in uiState.categories) {
                 Button(
+                    modifier = Modifier
+                        .weight(1.0f)
+                        .padding(5.dp),
                     onClick = { viewModel.selectCategory(category) }
                 ) {
                     Text(category)
