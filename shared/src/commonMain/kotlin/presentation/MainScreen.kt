a17832eb22e7c6b8c3efad1a63cfe81a46317c74
Nicolas Mahnic <nico.mahnic.ar@gmail.com>
2023-09-02 03:10:17 +0200

    revert aspect changes

--- shared/src/commonMain/kotlin/presentation/MainScreen.kt
+++ shared/src/commonMain/kotlin/presentation/MainScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -20,7 +19,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import domain.model.BirdImage
@@ -31,16 +29,7 @@ import io.kamel.image.asyncPainterResource
 fun BirdAppTheme(
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        colors = MaterialTheme.colors.copy(
-            primary = Color.Black
-        ),
-        shapes = MaterialTheme.shapes.copy(
-            small = AbsoluteCutCornerShape(0.dp),
-            medium = AbsoluteCutCornerShape(0.dp),
-            large = AbsoluteCutCornerShape(0.dp)
-        )
-    ) {
+    MaterialTheme {
         content()
     }
 }
